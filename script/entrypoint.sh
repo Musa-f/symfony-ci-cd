768fe9df9cb6e5ff3b71cbca9ae870faa2e6a770
musa-f <musaevafatima77@gmail.com>
2024-06-16 17:45:23 +0200

    Deploy

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -3,10 +3,10 @@
 cd /var/www/
 
 echo "Running migrations..."
-php bin/console doctrine:migrations:migrate --no-interaction
+php /var/www/bin/console doctrine:migrations:migrate --no-interaction
 
 echo "Loading fixtures..."
-php bin/console doctrine:fixtures:load --no-interaction
+php /var/www/bin/console doctrine:fixtures:load --no-interaction
 
 echo "Starting Apache..."
 exec apache2-foreground
