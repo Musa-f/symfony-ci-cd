391db032951c85bfa3baaf450017061911c146ac
musa-f <musaevafatima77@gmail.com>
2024-06-16 20:56:55 +0200

    --

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
 cd /var/www/
-
 php /var/www/bin/console cache:clear
-
-echo "Running migrations..."
 php /var/www/bin/console doctrine:migrations:migrate --no-interaction
-
-echo "Loading fixtures..."
 php /var/www/bin/console doctrine:fixtures:load --no-interaction
-
-echo "Starting Apache..."
 exec apache2-foreground
